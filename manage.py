af892e9a09eb7e15bbae5cdf5a0150bd5bff2bb7
XIAOHUI <HUI@163.COM>
2018-08-30 21:06:46 +0800

    添加csrf_token＆session存储配置到redis中

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask,session
 from flask_sqlalchemy import  SQLAlchemy
 from redis import StrictRedis
+from flask_wtf.csrf import CSRFProtect
+from flask_session import Session
 
 # 创建项目配置类
 class Config(object):
@@ -17,6 +19,20 @@ class Config(object):
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = "6379"
 
+    # 加密字符串
+    SECRET_KEY = "dIAWjw/xHnFH+MLXDshe98mbSH8QBchWS0bS36/WFpCZndzeSPdGb++3xdhPIYNJ"
+    # 调整session存储位置（存储到redis）
+    # session存储到那种数据库
+    SESSION_TYPE = "redis"
+    # 上面指明的数据库的实例对象
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # session数据需要加密
+    SESSION_USE_SIGNER = True
+    #  不设置永久存储
+    SESSION_PERMANENT = False
+    # 默认存储有效时常
+    PERMANENT_SESSION_LIFETIME = 86400*2
+
 # 1.创建app对象
 app = Flask(__name__)
 # 2.注册配置信息到app对象
@@ -25,6 +41,11 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 # 创建redis数据库对象
 redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+# ５.开启csrf后端验证保护机制
+#  提取cookie中的csrf_token和ajax里面的csrf_token进行比较
+csrf = CSRFProtect(app)
+# 6.创建session扩展类对象
+Session(app)
 
 
 @app.route("/")
