cabb6ca30bdf3191c2cafc36cbdbecb97eda15fc
XIAOHUI <HUI@163.COM>
2018-08-31 21:44:08 +0800

    返回模板文件＆返回图标文件

--- info/modules/index/__init__.py
+++ info/modules/index/__init__.py
@@ -2,7 +2,7 @@
 from flask import Blueprint
 
 # 1. 创建蓝图对象
-index_bp = Blueprint("index", __name__,url_prefix="/index")
+index_bp = Blueprint("index", __name__)
 
 # 切记：让index模块知道有views.py这个文件
 from .views import *
--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,5 +1,5 @@
 from . import index_bp
-from flask import session,current_app
+from flask import session,current_app,render_template
 from info import redis_store,models
 
 # 2.使用蓝图对象
@@ -7,9 +7,17 @@ from info import redis_store,models
 @index_bp.route("/")
 def index():
 
+    #
+    # redis_store.set("name", "laowang")
+    # current_app.logger.debug("记录日志")
+    # # 没有调整之前：　数据存在flask后端服务器　　只是讲session_id使用cookie传给客户端
+    # session["name"] = "James"
+    return render_template("index.html")
 
-    redis_store.set("name", "laowang")
-    current_app.logger.debug("记录日志")
-    # 没有调整之前：　数据存在flask后端服务器　　只是讲session_id使用cookie传给客户端
-    session["name"] = "James"
-    return "hello word"
+
+# 返回web的头像图标
+@index_bp.route("/favicon.ico")
+def facicon():
+    """返回图标"""
+    #  send_static_file:将static中的静态文件发送给浏览器
+    return current_app.send_static_file("news/favicon.ico")
